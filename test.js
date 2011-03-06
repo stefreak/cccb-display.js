6470c1b6a7ee9e86bdbd7f3ef4dd59aacaf5735c
Steffen Neubauer <stefreak@stefreak.de>
2011-03-06 23:24:28 +0100

    all new design

--- test.js
+++ test.js
@@ -3,11 +3,21 @@ display.DEBUG = true;
 
 var disp = new display.Display();
 
-disp.request({cmd: 2});
-for(var y=0; y<20; y++) {
-  for (var x=0; x<56; x+=3) {
-    console.log(x)
-    console.log(y)
-    disp.request({cmd:3, y:y, x:x, data:'BLA'});
+// clear the display
+disp.clear();
+
+function test() {
+  var x = 0;
+  for(var y=0; y<20; y++) {
+    for (var x=0; x<56; x+=3) {
+      disp.request({cmd:3, y:y, x:x, data:'BLA'});
+    }
   }
+  setTimeout(test, 5000);
 }
+function count(i) {
+  disp.request({cmd:3, y:0, x:0, data:i.toString(), resend:false}, function() {
+    count(++i);
+  });
+}
+count(0);
