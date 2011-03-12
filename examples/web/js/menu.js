0aea12c9631933a1d00805452389b40e63fcc215
Steffen Neubauer <stefreak@stefreak.de>
2011-03-13 00:34:11 +0100

    added comments in menu.js

--- examples/web/js/menu.js
+++ examples/web/js/menu.js
@@ -2,19 +2,32 @@
   function chpage(){
     var html, name;
 
+    // get name
     if (location.hash[0] === '#') {
       name = location.hash.substr(1);
-      html = $('script#' + name).html();
     }
-    if (!html)
-      html = $('script#default').html();
+
+    // validate name
+    switch(name) {
+      case 'write':
+      case 'draw':
+        break;
+      default:
+        name = 'default';
+    }
+
+    // get template
+    html = $('script#' + name).html();
 
     // apply
     $('#content').html(html);
 
-    if (name)
+    // append script
+    if (name !== 'default')
       $('body').append('<script src="/js/'+ name +'.js"></script>');
   }
+
+  // register events
   $(window).bind('hashchange', chpage);
   $(window).ready(chpage);
 })(jQuery);
