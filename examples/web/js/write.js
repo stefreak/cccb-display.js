b0818f4fcfb7dc80469707e3b64d9081a48a6cf6
Steffen Neubauer <stefreak@stefreak.de>
2011-03-12 19:38:00 +0100

    examples/web/write: only send the lines that changed

--- examples/web/js/write.js
+++ examples/web/js/write.js
@@ -1,15 +1,27 @@
 (function($){
+  var old = [""];
 
   function send(text) {
-    display.clear();
-    lines = text.split('\n');
+    var lines = text.split('\n');
+
     for (l in lines) {
-      display.request({cmd: 3, data: lines[l], y: l});
+      empty = "                                                              ";
+      line = lines[l];
+      if (old[l] !== line) {
+        if (line.length < 56)
+          line += empty.substr(0, 56 - line.length);
+        else
+          line = line.substr(0, 56);
+        display.request({cmd: 3, data: line, y: l});
+      }
     }
+
+    old = lines;
   }
 
   var textarea = $('textarea');
 
+  display.clear();
   $(window).bind('keyup', function() {
     send(textarea.val());
   });
